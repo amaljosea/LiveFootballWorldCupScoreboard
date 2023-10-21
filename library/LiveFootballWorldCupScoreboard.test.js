094a95dc7c8564fb0f5fe120cc1905a6f68a1937
amal <howareyouami@gmail.com>
2023-10-21 19:17:09 +0200

    updateScore test

--- library/LiveFootballWorldCupScoreboard.test.js
+++ library/LiveFootballWorldCupScoreboard.test.js
@@ -11,4 +11,11 @@ describe("LiveFootballWorldCupScoreboard", () => {
     scoreboard.startMatch("Team A", "Team B");
     expect(scoreboard.matches).toHaveLength(1);
   });
+
+  it("should update the score of a match", () => {
+    scoreboard.startMatch("Team A", "Team B");
+    scoreboard.updateScore(0, 2, 1);
+    expect(scoreboard.matches[0].homeScore).toBe(2);
+    expect(scoreboard.matches[0].awayScore).toBe(1);
+  });
 });
