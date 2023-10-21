662fcd401040c7dd1216108bd9e1701020685acb
amal <howareyouami@gmail.com>
2023-10-21 19:12:08 +0200

    Basic libriary

---
+++ library/LiveFootballWorldCupScoreboard.ts
@@ -0,0 +1,13 @@
+type Match = {
+  homeTeam: string;
+  awayTeam: string;
+  awayScore: number;
+};
+
+class LiveFootballWorldCupScoreboard {
+  constructor() {}
+
+  startMatch() {}
+}
+
+module.exports = { LiveFootballWorldCupScoreboard };
